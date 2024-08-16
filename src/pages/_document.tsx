ef7adc5d34904f856fb045121b8b4df3141a17b8
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-08-16 15:32:53 +0800

    [draft] add preview.css

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,10 +10,12 @@ export default function Document() {
       <link href="https://api.fontshare.com/v2/css?f[]=satoshi@700&f[]=general-sans@300,500&f[]=switzer@400&display=swap" rel="preload" as={"style"}/>
       <link href="https://api.fontshare.com/v2/css?f[]=satoshi@700&f[]=general-sans@300,500&f[]=switzer@400&display=swap" rel="stylesheet"/>
 
+      <link rel="preload" href={"/preview.css"} as={"style"}/>
+      <link rel="stylesheet" href={"/preview.css"} as={"style"}/>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
   );
-}
+};
