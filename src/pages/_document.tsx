8f19c0fc7630211579f6580c4fc1b6aa7e2c8eea
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-08-16 17:09:29 +0800

    add "cdn.fontshare.com" to preconnect

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
 
+      <link rel="preconnect" href="https://cdn.fontshare.com/wf/" crossOrigin={"anonymous"}/>
       <link rel="preconnect" href="https://api.fontshare.com" crossOrigin={"anonymous"}/>
 
       <link href="https://api.fontshare.com/v2/css?f[]=satoshi@700&f[]=general-sans@300,500&f[]=switzer@400&display=swap" rel="preload" as={"style"}/>
