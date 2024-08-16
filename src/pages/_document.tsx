38fb7c086b315ebf896d3269e231a9540c5d311a
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-08-16 22:16:02 +0800

    move fonts to a proper css file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,9 +8,6 @@ export default function Document() {
       <link rel="preconnect" href="https://cdn.fontshare.com/wf/" crossOrigin={"anonymous"}/>
       <link rel="preconnect" href="https://api.fontshare.com" crossOrigin={"anonymous"}/>
 
-      <link href="https://api.fontshare.com/v2/css?f[]=satoshi@700&f[]=general-sans@300,500&f[]=switzer@400&display=swap" rel="preload" as={"style"}/>
-      <link href="https://api.fontshare.com/v2/css?f[]=satoshi@700&f[]=general-sans@300,500&f[]=switzer@400&display=swap" rel="stylesheet"/>
-
       <link rel="preload" href={"/preview.css"} as={"style"}/>
       <link rel="stylesheet" href={"/preview.css"} as={"style"}/>
       <body>
