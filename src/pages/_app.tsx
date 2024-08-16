809f64ff73bb008e2e0899985c03f65e19e5a901
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-08-16 15:30:31 +0800

    minor tweaks on App function

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,5 @@ import type { AppProps } from "next/app";
 import "normalize.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+  return <Component {...pageProps} />
+};
