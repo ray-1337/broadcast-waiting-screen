8b3d5c4be05fd33ee97d06737f13a6665a57ad49
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-08-16 15:31:39 +0800

    [draft] add special fonts family

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+
+      <link rel="preconnect" href="https://api.fontshare.com" crossOrigin={"anonymous"}/>
+
+      <link href="https://api.fontshare.com/v2/css?f[]=satoshi@700&f[]=general-sans@300,500&f[]=switzer@400&display=swap" rel="preload" as={"style"}/>
+      <link href="https://api.fontshare.com/v2/css?f[]=satoshi@700&f[]=general-sans@300,500&f[]=switzer@400&display=swap" rel="stylesheet"/>
+
       <body>
         <Main />
         <NextScript />
