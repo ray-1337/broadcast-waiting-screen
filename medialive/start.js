c450242de4ed427fcdc446831fdcb8884a6d151b
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-08-19 07:24:39 +0800

    check if the stream is already running before starting

--- medialive/start.js
+++ medialive/start.js
@@ -1,13 +1,19 @@
-const { StartChannelCommand } = require("@aws-sdk/client-medialive");
+const { StartChannelCommand, DescribeChannelCommand } = require("@aws-sdk/client-medialive");
 
 const client = require("./client");
 
-const command = new StartChannelCommand({
-  ChannelId: process.env.AWS_MEDIALIVE_CHANNEL_ID
-});
+const ChannelId = process.env.AWS_MEDIALIVE_CHANNEL_ID;
+
+const command = new StartChannelCommand({ ChannelId });
 
 async function start() {
   try {
+    const channelStatus = await client.send(new DescribeChannelCommand({ ChannelId }));
+    if (channelStatus.State === "RUNNING") {
+      console.error("[AWS] The current streaming is already running.");
+      return;
+    };
+
     await client.send(command);
     console.log("[AWS] stream started.");
   } catch (error) {
