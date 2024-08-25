d30ae1e6d778029ff5dda09cc18453522d5a4e95
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-08-25 21:38:47 +0800

    explicitly set viewport content

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
+import { Fragment } from "react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+
 import "normalize.css";
 import "../styles/fonts.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Head>
+        <meta name="viewport" content="width=device-width, height=device-height, user-scalable=no, shrink-to-fit=no, initial-scale=1.0" />
+      </Head>
+
+      <Component {...pageProps} />
+    </Fragment>
+  );
 };
