a7c74a2243c7a7830bbf6a77c3c414ae54c3bf12
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-09-04 11:07:15 +0800

    include global.css on _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import { Fragment } from "react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+
 import "normalize.css";
+import "@/styles/global.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
